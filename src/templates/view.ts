90f13ea4c4f3733bb2b2d78aa9f9cede87b7906c
许致信 <13335309435@qq.com>
2024-04-10 13:45:04 +0800

    页面设置name

--- src/templates/view.ts
+++ src/templates/view.ts
@@ -5,7 +5,14 @@ export const viewTemplate = (folderPath: string) => {
       </div>
 </template>
 
-<script setup>
+<script lang="ts">
+// 页面名称
+export default {
+  name: "cms_${folderPath}_index"
+};
+</script>
+
+<script setup lang="ts">
       import { ${folderPath}Config } from "../config/config"
       import { ${folderPath}Controller } from "../controller/index"
       import { ${folderPath}Request } from "../request/request"
