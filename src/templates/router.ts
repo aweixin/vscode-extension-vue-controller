711509ff4448084fe1d011cac323519928a588c2
Alex Carter <13335309435@qq.com>
2024-04-29 23:21:16 +0800

    Update router.ts

--- src/templates/router.ts
+++ src/templates/router.ts
@@ -5,18 +5,21 @@ export const routerTemplate = (path: string, folderPath: string) => {
 
       const viewsName = "@/" + routerPath.slice(routerPath.indexOf("views")).join("/")
 
+
+      const path_name = viewsAfter.length ? '${viewsAfter.join("-")}-${folderPath}-index':'${folderPath}-index';
+
       const _name = folderPath + "Routes"
       return `import type { RouteRecordRaw } from "vue-router"
 // ${folderPath} 自定义路由 
 export const ${_name} : Array<RouteRecordRaw> = [
       {
             path: "${viewsAfter.join("/").replace("views", "")}/${folderPath}/index",
-            name: "${viewsAfter.join("-")}-${folderPath}-index",
+            name: "${path_name}",
             component: () => import("${viewsName}/${folderPath}/view/index.vue"),
             meta: {
                   index: 1,
                   pageTitle: "${folderPath}",
-                  breadcrumbs: ["${viewsAfter.join("-")}-${folderPath}-index"],
+                  breadcrumbs: ["${path_name}"],
             },
       },
 ]`
